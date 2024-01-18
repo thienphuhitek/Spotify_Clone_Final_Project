21741b23824d8ab319062f8c29f17972216c95f5
thienphuhitek <phuvo@hitek.com.vn>
2024-01-19 00:22:15 +0700

    FEATURE : add supabase provider

--- components/Box.tsx
+++ components/Box.tsx
@@ -5,14 +5,24 @@ interface BoxProps {
   className?: string;
 }
 
-const Box: React.FC<BoxProps> = ({ children, className }) => {
-  return (
-    <div
-      className={twMerge(`bg-neutral-900 rounded-lg h-fit w-full`, className)}
-    >
+const Box: React.FC<BoxProps> = ({ 
+  children,
+  className
+ }) => {
+  return ( 
+    <div 
+      className={twMerge(
+        `
+        bg-neutral-900 
+        rounded-lg 
+        h-fit 
+        w-full
+        `, 
+        className
+      )}>
       {children}
     </div>
   );
-};
-
+}
+ 
 export default Box;
